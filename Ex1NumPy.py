0a5027e972de66ec6288993fbb0e4519185d71fb
Pasquale <pasqualecolucci1004@gmail.com>
2025-03-22 00:12:35 +0100

    Add Ex1NumPy.py and Ex2NumPy.py
    These are two exercises to practice using the python NumPy library. The first is an exercise focused on one-dimensional arrays, while the second exercise focuses on two-dimensional arrays (matrices).

---
+++ Ex1NumPy.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+# Exercise 1: Creating and Manipulating Arrays
+#   - Create a NumPy array of 10 random numbers between 0 and 100.
+#   - Calculate the mean and standard deviation of the array.
+#   - Find the maximum and minimum values ​​in the array.
+#   - Add 5 to all elements of the array.
+#   - Sort the array in ascending and descending order.
+
+def init_array():
+    x = np.random.randint(0, 101, size = 10)
+    print("My array is: ", x)
+    return x
+
+def average(arr):
+    return np.average(arr)
+
+def standard_deviation(arr):
+    return np.std(arr)
+
+def findMax(arr):
+    return np.max(arr)
+
+def findMin(arr):
+    return np.min(arr) 
+
+def addFiveElem(arr):
+    return arr + 5 
+
+# Sorting in ascending order
+def sortAsc(arr):
+    return np.sort(arr)
+
+# Sorting in descending order
+def sortDes(arr):
+    return np.sort(arr)[::-1]
+
+arr = init_array()
+print("Average is", average(arr))
+print("Dev. std. is %.3f" %standard_deviation(arr))
+print("Max value is", findMax(arr))
+print("Min value is", findMin(arr))
+print("Array + 5 is", addFiveElem(arr))
+print("Sort in ascending order is", sortAsc(arr))
+print("Sort in descending order is", sortDes(arr))
---
+++ Ex2NumPy.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+# Create two 3x3 matrices with random numbers between 1 and 10.
+# Do the following:
+#   - Add the two matrices.
+#   - Product the two matrices.
+#   - Calculate the determinant of both matrices and print the largest.
+#   - Transpose both matrices.
+
+def init_matrix():
+    mat_1 = np.random.randint(1, 101, size = (3, 3))
+    mat_2 = np.random.randint(1, 101, size = (3, 3))
+    print("My first matrix is\n", mat_1)
+    print("My second matrix is\n", mat_2)
+    return mat_1, mat_2
+
+def sum(mat_1, mat_2): 
+    return mat_1 + mat_2
+
+def product(mat_1, mat_2):
+    return mat_1 @ mat_2
+
+def determinant(mat):
+    return np.linalg.det(mat)
+
+def transpose(mat):
+    return mat.T
+
+mat_1, mat_2 = init_matrix()
+print("The sum of mat_1\n", mat_1, "and mat_2\n", mat_2, "is\n", sum(mat_1, mat_2))
+print("The matrix product of mat_1\n", mat_1, "and mat_2\n", mat_2, "is\n", product(mat_1, mat_2))
+print("The determinant of mat_1\n", mat_1, "is", determinant(mat_1), "and mat_2\n", mat_2, "is", determinant(mat_2))
+print("The transpose matrix of mat_1\n", mat_1, "is\n", transpose(mat_1), "and transpose of mat_2\n", mat_2, "is\n", transpose(mat_2))
